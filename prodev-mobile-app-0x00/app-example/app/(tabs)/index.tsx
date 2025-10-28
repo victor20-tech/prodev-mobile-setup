8ff54435fcc9977fe57df364e0247044697fb86a
victor20-tech <victoradomako10@gmail.com>
2025-10-28 19:35:44 +0000

    feat: Add app-examples and updated the home screen

---
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>First App Created</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
