da8703bf6379e71b8e2a81f46d835737768cc95f
victor20-tech <victoradomako10@gmail.com>
2025-10-28 18:09:58 +0000

    Upadated the project setup using "npm run reset-project"

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>First App Created</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
